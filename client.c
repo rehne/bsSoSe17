b7ad1264fdfa8c3164a824a1c1c40fde9bbbe385
rehne <rene.honnen@smail.th-koeln.de>
2017-05-18 15:43:22 +0200

    client Verbindungsaufbau

--- client.c
+++ client.c
@@ -1,11 +1,32 @@
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int create_socket;
-int main() {
-    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0) > 0)) {
-        printf("Socket %i wurde angelegt\n", create_socket);
+int main(int argc, char **argv) {
+    int sock;
+    struct sockaddr_in sin;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0) > 0)) {
+        printf("Socket %i wurde angelegt\n", sock);
     } else {
         printf("Socket konnte nicht angelegt werden.\n");
     }
+
+    int sin_len = sizeof(sin);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(1500);
+
+    if(connect(sock, (struct sockaddr *) &sin, sizeof(sin)) == 0){
+      printf("Verbindung mit dem Server %s hergestellt\n",
+              inet_ntoa(sin.sin_addr));
+    } else {
+      printf("keine Verbindung hergestellt\n");
+    }
+
+    close(sock);
+    printf("");
+    return (EXIT_SUCCESS);
 }
